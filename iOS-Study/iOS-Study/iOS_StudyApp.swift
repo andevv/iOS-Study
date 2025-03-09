32c32ef87154a9779a3370b9206db5f0e7a9d72f
andev <andh1006@naver.com>
2025-03-09 20:22:08 +0900

    Initial project

---
+++ iOS-Study/iOS-Study/iOS_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_StudyApp.swift
+//  iOS-Study
+//
+//  Created by andev on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS-Study/iOS-StudyTests/iOS_StudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_StudyTests.swift
+//  iOS-StudyTests
+//
+//  Created by andev on 3/9/25.
+//
+
+import Testing
+@testable import iOS_Study
+
+struct iOS_StudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
