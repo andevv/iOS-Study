a88ad2f509b616269c58a881461abd7f93f1cdee
andev <andh1006@naver.com>
2025-03-13 21:11:26 +0900

    CustomButton

--- iOS-Study/iOS-Study/CustomButton.swift
+++ iOS-Study/iOS-Study/CustomButton.swift
@@ -5,4 +5,28 @@
 //  Created by andev on 3/13/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomButton: View {
+    let placeholder: String
+    
+    var body: some View {
+        Button(action: {
+            print("버튼이 눌러졌습니다!")
+        }, label: {
+            Text(placeholder)
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding()
+        })
+    }
+}
+
+
+#Preview {
+    CustomButton(placeholder: "버튼 이름이 바뀌었습니다!")
+}
